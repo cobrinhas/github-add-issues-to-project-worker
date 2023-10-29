49057c49ff07042001e482d4fef37630e7965e8f
RuteSantos <1160663@isep.ipp.pt>
2023-10-29 19:42:21 +0000

    feat: setup project using worker brick

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { Env, fromEnv } from "./config";
+
+
+
+
+export default {
+	async scheduled(
+		controller: ScheduledController,
+		env: Env,
+		ctx: ExecutionContext
+	): Promise<void> {
+		const config = fromEnv(env);
+
+		console.info(`Hello ${config.awesomeSecret}!`);
+
+		return Promise.resolve();
+	},
+};
