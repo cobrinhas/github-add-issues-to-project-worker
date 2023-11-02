27d949404377f9601780cc346c90b5e8dc18defa
RuteSantos <1160663@isep.ipp.pt>
2023-11-02 19:58:45 +0000

    feat: add try catch

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { GraphqlResponseError } from '@octokit/graphql';
 import { Env, fromEnv } from './config';
 import { IssueId, IssueState, IssueVisibility, ProjectInfo } from './data';
 import { assigneeTo, createdBy } from './issues';
@@ -11,39 +12,45 @@ export default {
 	): Promise<void> {
 		const config = fromEnv(env);
 
-		const allOpenIssues: IssueId[] = await getAllIssues(
-			config.githubAccessToken,
-			config.githubIssuesOwnerUsernames,
-			config.queryPageSize
-		);
+		try {
+			const allOpenIssues: IssueId[] = await getAllIssues(
+				config.githubAccessToken,
+				config.githubIssuesOwnerUsernames,
+				config.queryPageSize
+			);
 
-		if (allOpenIssues.length == 0) {
-			return Promise.resolve();
+			if (allOpenIssues.length == 0) {
+				console.log('no open issues');
+				return Promise.resolve();
+			}
+
+			const projectInfo: ProjectInfo = await getProjectByNumber(
+				config.githubAccessToken,
+				config.githubProjectOwnerUsername,
+				config.githubProjectNumber,
+				config.queryPageSize
+			);
+
+			const projectIssuesIds: IssueId[] = projectInfo.issues;
+
+			const issues2Add: IssueId[] = [
+				...new Set(allOpenIssues.filter((x) => !projectIssuesIds.includes(x)))
+			];
+
+			if (issues2Add.length == 0) {
+				console.log('no open issues to be added');
+				return Promise.resolve();
+			}
+
+			return await addIssues2Project(
+				config.githubAccessToken,
+				config.githubProjectOwnerUsername,
+				projectInfo.id,
+				issues2Add
+			).then();
+		} catch (error) {
+			console.error(error);
 		}
-
-		const projectInfo: ProjectInfo = await getProjectByNumber(
-			config.githubAccessToken,
-			config.githubProjectOwnerUsername,
-			config.githubProjectNumber,
-			config.queryPageSize
-		);
-
-		const projectIssuesIds: IssueId[] = projectInfo.issues;
-
-		const issues2Add: IssueId[] = [
-			...new Set(allOpenIssues.filter((x) => !projectIssuesIds.includes(x)))
-		];
-
-		if (issues2Add.length == 0) {
-			return Promise.resolve();
-		}
-
-		return addIssues2Project(
-			config.githubAccessToken,
-			config.githubProjectOwnerUsername,
-			projectInfo.id,
-			issues2Add
-		).then();
 	}
 };
 
